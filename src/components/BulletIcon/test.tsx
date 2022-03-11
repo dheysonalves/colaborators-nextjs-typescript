5737c245febaf35e94c93eb76c5b96fd9ba7a752
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-03-10 22:25:17 -0300

    feat: update the test for the bullet icon

--- src/components/BulletIcon/test.tsx
+++ src/components/BulletIcon/test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { IconType } from 'react-icons'
 import { render } from '@testing-library/react'
 import { ThemeProvider } from '@emotion/react'
 import { FiFileText } from 'react-icons/fi'
 
 import BulletIcon from '.'
 import { theme } from '../../styles/theme/theme'
-import { IconType } from 'react-icons'
 
 const setup = (Icon: IconType, isSmall: boolean, size: number) => {
 	const utils = render(
